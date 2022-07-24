3e406eb23d47070abc1e7b7ea382975d0c16a455
Diniz <barrosjunior26@outlook.com.br>
2022-07-24 20:18:44 -0300

    Foi adicionado a pasta Bissexto que contém códigos de linguagem de programação em C# e foi editado os textos do arquivo readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
+# Analisando se o ano informado pelo usuário é bissexto 📅
+
+## Como saber se um ano é bissexto
+
+Para saber se um ano é bissexto, devemos verificar se ele se encaixa em um dos casos:
+
+**Caso 1) É um número divisível por 4, mas não é divisível por 100.**
+**Caso 2) É um número divisível por 4, por 100 e por 400.**
 
---
+++ anoBissexto/anoBissexto/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Excercico21
+{
+    class Ano_Bissexto
+    {
+        public static void Main(String[] args)
+        {
+            //Faça um algoritmo que leia um determinado ano e mostre se ele é ou não BISSEXTO.
+
+            //Solicita ao usuário ano
+            Console.Write("Informe o ano que você deseja verificar se é ou não bissexto: ");
+            int ano = int.Parse(Console.ReadLine());
+
+            //Processamento dos dados e condições que irão verificar se o ano é Bissexto
+            if(ano % 4 == 0)
+            {
+                Console.WriteLine("O ano informado é BISSEXTO!");
+            }else if(ano % 100 == 0)
+            {
+                Console.WriteLine("O ano informado é BISSEXTO!");
+            }else if(ano % 400 == 0)
+            {
+                Console.WriteLine("O ano informado é BISSEXTO!");
+            }
+            else
+            {
+                Console.WriteLine("O ano informado não é BISSEXTO!");
+            }
+        }
+    }
+}
