15f20951e02662a7cab0004661e0d1eb5146b7bf
Diniz <barrosjunior26@outlook.com.br>
2022-07-24 20:24:27 -0300

    Neste commit foi adicionado uma linha de declaração de variáveis, adicionado o laço de repetição do while para que o usuário possa realizar uma nova operação

--- anoBissexto/anoBissexto/Program.cs
+++ anoBissexto/anoBissexto/Program.cs
@@ -6,25 +6,35 @@
         {
             //Faça um algoritmo que leia um determinado ano e mostre se ele é ou não BISSEXTO.
 
-            //Solicita ao usuário ano
-            Console.Write("Informe o ano que você deseja verificar se é ou não bissexto: ");
-            int ano = int.Parse(Console.ReadLine());
+            //Declaração de variáveis
+            int opcao = 0;
 
-            //Processamento dos dados e condições que irão verificar se o ano é Bissexto
-            if(ano % 4 == 0)
+            do
             {
-                Console.WriteLine("O ano informado é BISSEXTO!");
-            }else if(ano % 100 == 0)
-            {
-                Console.WriteLine("O ano informado é BISSEXTO!");
-            }else if(ano % 400 == 0)
-            {
-                Console.WriteLine("O ano informado é BISSEXTO!");
-            }
-            else
-            {
-                Console.WriteLine("O ano informado não é BISSEXTO!");
-            }
+                //Solicita ao usuário ano
+                Console.Write("Informe o ano que você deseja verificar se é ou não bissexto: ");
+                int ano = int.Parse(Console.ReadLine());
+
+                //Processamento dos dados e condições que irão verificar se o ano é Bissexto
+                if (ano % 4 == 0)
+                {
+                    Console.WriteLine("O ano informado é BISSEXTO!");
+                }
+                else if (ano % 100 == 0)
+                {
+                    Console.WriteLine("O ano informado é BISSEXTO!");
+                }
+                else if (ano % 400 == 0)
+                {
+                    Console.WriteLine("O ano informado é BISSEXTO!");
+                }
+                else
+                {
+                    Console.WriteLine("O ano informado não é BISSEXTO!");
+                }
+                Console.Write("Digite 1 para NOVA OPERAÇÃO ou 0 para SAIR: ");
+                opcao = int.Parse(Console.ReadLine());
+            }while(opcao != 0);
         }
     }
 }
